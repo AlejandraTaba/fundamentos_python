ff2f30155c057ace56d45886d1f34537a1117f4b
AlejandraTaba <alejaydante2@gmail.com>
2025-04-05 17:52:38 -0500

    primeros pasos de python

---
+++ par_impar.py
@@ -0,0 +1,5 @@
+num = int(input("Digite un numero: "))
+if num % 2 == 0:
+    print("número es par.")
+else:
+    print("es impar.")
---
+++ sumas.py
@@ -0,0 +1,4 @@
+#Suma de dos numeros
+a = 5
+b = 3
+print(a + b)
